f6cfaa50be5014910c3ebbeb877b0e40a3035bcf
Priyanka <priyanka.23219@gmail.com>
2021-05-09 17:13:20 +0530

    Ascii value

---
+++ src/aciivalue/AciiValue.java
@@ -0,0 +1,10 @@
+
+public class AciiValue {
+    public static void main(String[] args) {
+         char ch = 'a';
+        int ascii = ch;
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+       
+    }
+    
+}
